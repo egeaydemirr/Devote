d5ed1a3272fae6a7c47f8d8f2b959a0b005b3479
Ege Aydemir <egeaydemirr97@gmail.com>
2022-10-29 18:38:08 +0300

    Managed Object Context with CoreData

---
+++ Devote/Utiliy/Constant.swift
@@ -0,0 +1,20 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Ege Aydemir on 29.10.2022.
+//
+
+import SwiftUI
+
+// MARK: - Formatter
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+// MARK: - UIX 
